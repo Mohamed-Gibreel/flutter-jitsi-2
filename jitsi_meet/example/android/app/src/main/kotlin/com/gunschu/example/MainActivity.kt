8e3d91aa99d78b56ad331dc11cf83704cedaa80f
Mohamed-Gibreel <m.adilgibreel@gmail.com>
2021-08-09 11:04:42 +0400

    Background activity in android now works

--- jitsi_meet/example/android/app/src/main/kotlin/com/gunschu/example/MainActivity.kt
+++ jitsi_meet/example/android/app/src/main/kotlin/com/gunschu/example/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.gunschu.example
 
+import android.content.Context
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull
+import com.gunschu.jitsi_meet.JitsiMeetPluginActivity
+import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "pip/fullscreen"
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "fullscreen"){
+                var intent = Intent(context, JitsiMeetPluginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                context?.startActivity(intent)
+            }
+        }
+    }
 }
--- jitsi_meet/example/android/app/src/main/kotlin/com/gunschu/jitsi_meet_example/MainActivity.kt
+++ jitsi_meet/example/android/app/src/main/kotlin/com/gunschu/jitsi_meet_example/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.gunschu.jitsi_meet_example
 
-import androidx.annotation.NonNull;
+import android.content.Context
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import androidx.annotation.NonNull
+import com.gunschu.jitsi_meet.JitsiMeetPluginActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "pip/fullscreen"
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "fullscreen"){
+                var intent = Intent(context, JitsiMeetPluginActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
+                context?.startActivity(intent)
+            }
+        }
     }
 }
+
